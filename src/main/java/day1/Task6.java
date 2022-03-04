f1817c0adc64bbe6748af7f95522dc94ee53d805
eq-Ethereal <dlikhanov@alfabank.ru>
2022-03-05 01:01:59 +0500

    Day-1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,13 @@
 package day1;
 
+import java.util.Random;
+
 public class Task6 {
     public static void main(String[] args) {
-
+        Random r = new Random();
+        int k = r.nextInt(11);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(k * i);
+        }
     }
 }
