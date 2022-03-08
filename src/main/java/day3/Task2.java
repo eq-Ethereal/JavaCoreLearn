d19bc20b20430b2f0fbe709d42ced3975a5d2046
eq-Ethereal <dlikhanov@alfabank.ru>
2022-03-08 12:09:51 +0500

    Day-3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+            double x = scanner.nextDouble();
+            double y = scanner.nextDouble();
+            if (y != 0.0) {
+                System.out.print(x / y + "\n");
+            } else {
+                flag = false;
+            }
+        }
     }
 }
